ba12f8e5b07f8e778fa3056b31bd1bbd0bfedbc3
marforitheus-a11y <marforitheus@gmail.com>
2025-11-13 18:22:32 -0300

    Update firebase-config.js

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,25 +1,21 @@
-// js/firebase-config.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Aguarda o módulo carregar
-const { initializeApp, getFirestore } = window.firebaseModules;
-
-// --- COLE SUA CONFIGURAÇÃO DO FIREBASE AQUI ---
-// Vai parecer com isso:
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD...",
-  authDomain: "simulado-ogmo.firebaseapp.com",
-  projectId: "simulado-ogmo",
-  storageBucket: "simulado-ogmo.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyAYi7oQ6oyS_fQS-gGuGT495NdxfMcffY0",
+  authDomain: "capatazia-4391a.firebaseapp.com",
+  projectId: "capatazia-4391a",
+  storageBucket: "capatazia-4391a.firebasestorage.app",
+  messagingSenderId: "248581392094",
+  appId: "1:248581392094:web:ecb618ca575f1806bfe44f",
+  measurementId: "G-47R4KNRSQF"
 };
-// ---------------------------------------------
 
-// Inicializa o Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// Disponibiliza o banco de dados globalmente
-window.db = db;
-
-console.log("Firebase conectado com sucesso!");
+const analytics = getAnalytics(app);
